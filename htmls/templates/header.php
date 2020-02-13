885314d8b4e24d516805729fe911cba3232b7a79
Dave Peterson <davpeterson@zoho.com>
2020-02-12 20:39:18 -0800

    add type

--- htmls/templates/header.php
+++ htmls/templates/header.php
@@ -5,9 +5,9 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- css -->
-		<link href="/css/bootstrap4/bootstrap.min.css?v<?=filemtime($_SERVER['DOCUMENT_ROOT'].'/css/bootstrap4/bootstrap.min.css')?>" rel="stylesheet">
-		<link href="/css/style.css?v<?=filemtime($_SERVER['DOCUMENT_ROOT'].'/css/style.css')?>" rel="stylesheet">
-		<link href="/images/fontawesome-5.2.0/css/all.css" rel="stylesheet"> <!--load all styles -->
+		<link href="/css/bootstrap4/bootstrap.min.css?v<?=filemtime($_SERVER['DOCUMENT_ROOT'].'/css/bootstrap4/bootstrap.min.css')?>" rel="stylesheet" type="text/css"/>
+		<link href="/images/fontawesome-5.2.0/css/all.css?v<?=filemtime($_SERVER['DOCUMENT_ROOT'].'/images/fontawesome-5.2.0/css/all.css')?>" rel="stylesheet" type="text/css"/> <!-- i know, should probably split js/css into their own folders, but why? -->
+		<link href="/css/style.css?v<?=filemtime($_SERVER['DOCUMENT_ROOT'].'/css/style.css')?>" rel="stylesheet" type="text/css"/>
 		<!-- title -->
 		<title>Dave's Live Recordings</title>
 	</head>
