08b02eb4cbefdf695bdeff4487fe4e5dd79edc8b
Dave Peterson <davpeterson@zoho.com>
2020-02-12 20:38:46 -0800

    add javascript descriptor

--- htmls/templates/footer.php
+++ htmls/templates/footer.php
@@ -4,5 +4,5 @@
 			</div>
 		</footer>
 		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="/js/jquery/jquery-3.3.1.min.js"></script>
-		<script src="/js/bootstrap4/bootstrap.min.js"></script>
+		<script src="/js/jquery/jquery-3.3.1.min.js" type="text/javascript"></script>
+		<script src="/js/bootstrap4/bootstrap.min.js" type="text/javascript"></script>
