ab4a6a5a8e36994eb064b4bf78033e829a77533b
Dave Peterson <davpeterson@zoho.com>
2020-09-09 00:03:44 -0700

    comment-out scroll-function stuff, caused console errors -- Uncaught TypeError: document.getElementById(...) is null

--- www/index.php
+++ www/index.php
@@ -14,10 +14,9 @@
 
 		<script>
 		$(document).ready(function(){
-			console.warn('document.ready');
-			// When the user scrolls down 20px from the top of the document, show the button
-			window.onscroll = function() {scrollFunction()};
-
+//			// When the user scrolls down 20px from the top of the document, show the button (??)
+//			window.onscroll = function() {scrollFunction()};
+	
 			$('.regenerate-shows button').on('click', function(){
 				$.post('/ajax/backgroundTask.php');
 			});
@@ -28,13 +27,13 @@
 				window.location.href = "/showlist.php?s=s";
 			});
 		});
-		function scrollFunction() {
-			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-				document.getElementById("up-button").style.display = "block";
-			} else {
-				document.getElementById("up-button").style.display = "none";
-			}
-		}
+//		function scrollFunction() {
+//			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//				document.getElementById("up-button").style.display = "block";
+//			} else {
+//				document.getElementById("up-button").style.display = "none";
+//			}
+//		}
 		function toTheTop(){
 			document.body.scrollTop = 0; // For Safari
 			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
